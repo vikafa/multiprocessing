df9095fa38d174ab7e2de5bb8dd9dc832cd5ae86
Shilkina Viktoriya <222769@edu.fa.ru>
2024-04-23 20:55:31 +0300

    Changes_seminar

--- matrix4.py
+++ matrix4.py
@@ -1,8 +1,10 @@
 import random
+
 from multiprocessing import Pool, cpu_count
+
 import numpy as np
 
-# Функция перемножения элементов матриц
+
 def element(index, A, B):
     i, j = index
     res = 0
@@ -11,41 +13,50 @@ def element(index, A, B):
         res += A[i][k] * B[k][j]
     return res
 
-# Функция для распараллеливания вычислений
-def parallel_multiply_matrices(A, B):
-    if len(A[0]) != len(B):
-        raise ValueError("Количество столбцов матрицы A должно быть равно количеству строк матрицы B")
 
-    # Создаем список индексов для каждого элемента результирующей матрицы
+def parallel_multiply_matrices(A, B):
+    if len(A) == 0 or len(B) == 0:
+        raise ValueError("Matrices cannot have zero dimensions")
+    if len(A[0]) != len(B):
+        raise ValueError("The number of columns in matrix A must be equal to the number of rows in matrix B")
+    if len(A) != len(A[0]):
+        raise ValueError("Matrix A must be square")
+    if len(B) != len(B[0]):
+        raise ValueError("Matrix B must be square")
+
     indices = [(i, j) for i in range(len(A)) for j in range(len(B[0]))]
 
-    # Создаем пул процессов, используя количество доступных ядер процессора
     with Pool(cpu_count()) as pool:
-        # Выполняем перемножение элементов матриц параллельно с помощью функции element
         result_elements = pool.starmap(element, [(index, A, B) for index in indices])
 
-    # Преобразуем список результатов в матрицу
     result_matrix = np.reshape(result_elements, (len(A), len(B[0])))
 
     return result_matrix
 
+
 if __name__ == '__main__':
-    a, b = map(int, input("For matrix1, a,b").split())
-    c, d = map(int, input("For matrix2, c,d").split())
-    # Создаем две случайные матрицы размером 5x5
-    A = np.random.randint(0, 10, size=(a, b))
-    B = np.random.randint(0, 10, size=(c, d))
+    while True:
+        try:
+            a, b = map(int, input("Enter dimensions of matrix A (a, b): ").split())
+            c, d = map(int, input("Enter dimensions of matrix B (c, d): ").split())
+            if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+                raise ValueError("Dimensions must be positive integers")
+            break
+        except ValueError as e:
+            print(e)
+
+    A = np.random.randint(0, 10, size=(a, a))
+    B = np.random.randint(0, 10, size=(b, b))
 
     try:
-        # Выполняем перемножение матриц с использованием многопроцессорности
         result = parallel_multiply_matrices(A, B)
 
-        # Выводим результат
         print("Matrix A:")
         print(A)
         print("\nMatrix B:")
         print(B)
         print("\nResult:")
         print(result)
+
     except ValueError as e:
         print(e)
